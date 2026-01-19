280de4b58b3b6068545a7aaf2bb2b3992aa3f73c
artem <sutalkuar808typ@gmail.com>
2026-01-19 16:18:34 +0200

    lambda

---
+++ 1_advanced_python/other/debag_practice/main.py
@@ -0,0 +1,12 @@
+def calculate_sum(a, b):
+    res = a + b
+    return res
+
+def main():
+    x = 5
+    y = 3
+    total = calculate_sum(x, y)
+    print(total)
+
+if __name__ == "__main__":
+    main()
---
+++ 1_advanced_python/other/lambda.py
@@ -0,0 +1,17 @@
+students = [
+    {'name': 'Марія', 'grade': 92},
+    {'name': 'Іван', 'grade': 85},
+    {'name': 'Петро', 'grade': 78}
+]
+
+print(sorted(students, key=lambda student: student['grade'], reverse=True))
+words = ['кіт', 'собак', 'слон', 'папуга']
+print(max(words, key=lambda word: len(word)))
+
+# print(list(map(lambda value: (value*9/5)+32, [25, 30, 0, 11, 44, 12])))
+
+# nums = [15, 5, 11, 25, 6, 11, 2, 3, 4, 0, 16]
+# bigger_filtered =
+# print(bigger_filtered)
+# squared =
+# print(list(map(lambda value: pow(value, 2), list(filter(lambda value: value > 10, nums)))))
