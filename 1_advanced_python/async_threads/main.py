b0ff6948672689b6f6609f658d18d0d0460ca3cd
artem <sutalkuar808typ@gmail.com>
2026-01-20 10:05:33 +0200

    generators_iterators

--- 1_advanced_python/async_threads/main.py
+++ 1_advanced_python/async_threads/main.py
@@ -1,2 +1,20 @@
 import time
+import asyncio
 
+async def func1():
+    await time.sleep(10)
+    await print("Func 1 ended")
+
+
+async def func2():
+    await time.sleep(10)
+    await print("Func 2 ended")
+
+while True:
+    choice = input()
+    if choice == '1':
+        func1()
+        continue
+    elif choice == '2':
+        func2()
+        continue
